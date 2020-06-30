fe22a351370314b46f7914fe7fdaaf0fe8cdaaa2
Gaby Scheleider <gabyscheleider@gmail.com>
2020-06-30 18:40:27 -0300

    Iniciando

---
+++ src/whatsappbot.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+class WhatsappBot:
+    def __init__(self):
+        # Parte 1 - A mensagem que você quer enviar
+        self.mensagem = "Isso é um teste, não entre em pánico https://www.youtube.com"
+        # Parte 2 - Nome dos grupos ou pessoas a quem você deseja enviar a mensagem
+        self.grupos_ou_pessoas = ["나는🌸アキロアユミ/ 지가연/地佳緣","나는🌸アキロアユミ/ 지가연/地佳緣"]
+        options = webdriver.ChromeOptions()
+        options.add_argument('lang=pt-br')
+        self.driver = webdriver.Chrome(
+            executable_path=r'./chromedriver.exe', chrome_options=options)
+
+    def EnviarMensagens(self):
+        self.driver.get('https://web.whatsapp.com')
+        time.sleep(30)
+        for grupo_ou_pessoa in self.grupos_ou_pessoas:
+            campo_grupo = self.driver.find_element_by_xpath(
+                f"//span[@title='{grupo_ou_pessoa}']")
+            time.sleep(3)
+            campo_grupo.click()
+            chat_box = self.driver.find_element_by_class_name('_13mgZ')
+            time.sleep(3)
+            chat_box.click()
+            chat_box.send_keys(self.mensagem)
+            botao_enviar = self.driver.find_element_by_xpath(
+                "//span[@data-icon='send']")
+            time.sleep(3)
+            botao_enviar.click()
+            time.sleep(5)
+
+
+bot = WhatsappBot()
+bot.EnviarMensagens()
